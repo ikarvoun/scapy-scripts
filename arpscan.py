2209d84f10a385bee732d046810c786508663244
ikarvoun <rick@karvouniaris.com>
2023-02-11 22:30:17 +1100

    Add files via upload

---
+++ arpscan.py
@@ -0,0 +1,25 @@
+from scapy.all import srp
+from scapy.all import Ether, ARP, conf
+
+import sys
+
+def arping(iprange="192.168.20.0/24"):
+    """Arping function takes IP Address or Network, returns nested mac/ip list"""
+
+    #conf, verb = 0
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=iprange), timeout=2)
+
+    collection = []
+    for snd, rcv in ans:
+        result = rcv.sprintf(r"%ARP.psrc% %Ether.src%").split()
+        collection.append(result)
+    return collection
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        for ip in sys.argv[1:]:
+            print("arping", ip)
+            print(arping(ip))
+
+    else:
+        print(arping())
